38db1471db0e9c77ec63c41b48bca972291f2b7a
Mathew Lopez <mathewlopez10@gmail.com>
2025-03-13 16:33:48 -0400

    Notes and button gap fix

--- client/src/smallComponents/Notes/index.jsx
+++ client/src/smallComponents/Notes/index.jsx
@@ -84,18 +84,21 @@ function SmallNotes({ notes }) {
                         + Create New Note
                     </button>}
 
-                    <h3 
-                        className='text-white-50 mt-1'
-                        style={{ textOverflow: 'ellipsis', whiteSpace: 'wrap' }}
-                    >
-                        {!notes || notes.length === 0 ? 
-                            <>
+                    {(!notes || notes.length === 0) && 
+                        <h3 
+                            className='text-white-50 mt-1'
+                            style={{ textOverflow: 'ellipsis', whiteSpace: 'wrap' }}
+                        >
                                 No notes available. <br />
                                 Start nesting your thoughts today!
-                            </>
-                            : ''}
-                    </h3>
-                    <p className='text-white-50'>{searchTerm && notes ? `All notes matching "${searchTerm}" are displayed below.` : ''}</p>
+                        </h3>
+                    }
+
+                    {(searchTerm && notes) && 
+                        <p className='text-white-50 m-0'>
+                            All notes matching "{searchTerm}" are displayed below.
+                        </p>
+                    }    
 
                     <div>
                         <ul className="list-group list-group-flush">
